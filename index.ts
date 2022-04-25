9f771cbfc8d2ae7835de51d47065f73d90f6a284
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-25 08:48:18 +0400

    support recurring events

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { PageEntity, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
 import ical from 'node-ical';
 import axios from 'axios';
 import { getDateForPage, getDateForPageWithoutBrackets } from 'logseq-dateutils';
+import moment from 'moment-timezone'
 
 // const md = require('markdown-it')().use(require('markdown-it-mark'));
 
@@ -112,17 +113,14 @@ function sortDate(data) {
   )
 }
 async function findDate(preferredDateFormat) {
-  const hello = 1
   if (await logseq.Editor.getCurrentPage() != null) {
     if ((await logseq.Editor.getCurrentPage())['journal?'] == false) {
-      // console.log(await logseq.Editor.getCurrentPage()['journal?'])
       const date = getDateForPageWithoutBrackets(new Date(), preferredDateFormat)
       logseq.App.showMsg("Filtering Calendar Items for " + date)
       // insertJournalBlocks(hello, preferredDateFormat, calendarName, settings, date)
       return date
     }
     else {
-      // console.log(await logseq.Editor.getCurrentPage()['journal?'])
       const date = (await logseq.Editor.getCurrentPage()).name
       logseq.App.showMsg(`Filtering Calendar Items for ${date}`)
       return date
@@ -137,12 +135,44 @@ function rawParser(rawData) {
   var eventsArray = []
   var rawDataV2 = ical.parseICS(rawData)
   for (const dataValue in rawDataV2) {
-    eventsArray.push(rawDataV2[dataValue]); //simplifying results, credits to https://github.com/muness/obsidian-ics for this implementations
+    
+    const event = rawDataV2[dataValue]
+    if (typeof event.rrule == 'undefined') {
+      eventsArray.push(rawDataV2[dataValue]); //simplifying results, credits to https://github.com/muness/obsidian-ics for this implementations
+    }
+    else {
+      const dates = event.rrule.between(new Date(2021, 0, 1, 0, 0, 0, 0), new Date(2023, 11, 31, 0, 0, 0, 0))
+      console.log(dates)
+      if (dates.length === 0) continue;
+
+      console.log('Summary:', event.summary);
+      console.log('Original start:', event.start);
+      console.log('RRule start:', `${event.rrule.origOptions.dtstart} [${event.rrule.origOptions.tzid}]`)
+
+      dates.forEach(date => {
+          let newDate
+          if (event.rrule.origOptions.tzid) {
+              // tzid present (calculate offset from recurrence start)
+              const dateTimezone = moment.tz.zone('UTC')
+              const localTimezone = moment.tz.guess()
+              const tz = event.rrule.origOptions.tzid === localTimezone ? event.rrule.origOptions.tzid : localTimezone
+              const timezone = moment.tz.zone(tz)
+              const offset = timezone.utcOffset(date) - dateTimezone.utcOffset(date)
+              newDate = moment(date).add(offset, 'minutes').toDate()
+          } else {
+              // tzid not present (calculate offset from original start)
+              newDate = new Date(date.setHours(date.getHours() - ((event.start.getTimezoneOffset() - date.getTimezoneOffset()) / 60)))
+          }
+          const start = moment(newDate)
+          const secondaryEvent = { ...event, start: start["_d"]}
+          eventsArray.push(secondaryEvent)
+      })
+
+      console.log('-----------------------------------------------------------------------------------------');
+    }
   }
   console.log(eventsArray)
-  // console.log(sortDate(eventsArray))
   return sortDate(eventsArray);
-  // return eventsArray
 }
 
 function templateFormatter(template, description = "No Description", date = "No Date", start = "No Start", end = "No End", title = "No Title", location = "No Location") {
@@ -164,7 +194,6 @@ function templateFormatter(template, description = "No Description", date = "No 
   var templatex1 = template
 
   for (const substitute in subsitutions) {
-    console.log(`This is a test of ${substitute}`)
     let template2 = templatex1.replace(substitute, subsitutions[substitute])
     let template3 = template2.replace(substitute.toLowerCase(), subsitutions[substitute])
     templatex1 = template3
@@ -174,7 +203,6 @@ function templateFormatter(template, description = "No Description", date = "No 
 
 function formatTime(rawTimeStamp, settings) {
   let formattedTimeStamp = new Date(rawTimeStamp)
-  // console.log(formattedTimeStamp)
   let initialHours = formattedTimeStamp.getHours()
   let hours;
   if (initialHours == 0) {
@@ -222,10 +250,6 @@ async function insertJournalBlocks(data, preferredDateFormat: string, calendarNa
       let endTime = formatTime(data[dataKey]["end"], settings)
       let location = data[dataKey]["location"]
       let summary
-      // if (data[dataKey]["summary"]["val"]){
-      //   summary = data[dataKey]["summary"]["val"]
-      // }
-      // else{
       summary = data[dataKey]["summary"]
       // }
       // using user provided template
@@ -256,14 +280,12 @@ async function openCalendar2(calendarName, url, settings) {
     const preferredDateFormat = userConfigs.preferredDateFormat;
     logseq.App.showMsg("Fetching Calendar Items")
     let response2 = await axios.get(url)
+    console.log(response2)
     var hello = await rawParser(response2.data)
-    console.log(hello)
-    console.log("Does anything show up here?")
     const date = await findDate(preferredDateFormat)
     insertJournalBlocks(hello, preferredDateFormat, calendarName, settings, date)
   }
   catch (err) {
-    console.log("Does anything else show up here?")
     if (`${err}` == `Error: Request failed with status code 404`) {
       logseq.App.showMsg("Calendar not found: Check your URL")
     }
